7da0d2501b987457de011055313be261af4184d9
ZHamburglar <kollinbrandenburg@gmail.com>
2018-11-27 13:46:59 -0600

    simple file creation and .json send

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,39 @@
-const axios = require('axios')
+const axios = require('axios');
+const fs = require('fs');
+var path = require('path');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  axios.get('https://data.austintexas.gov/resource/asyh-u6ja.json')
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-  res.status(200).send('woooooo');
+	// axios.get('https://data.austintexas.gov/resource/asyh-u6ja.json')
+	// .then(function (response) {
+	// 	console.log(response);
+	// })
+	// .catch(function (error) {
+	// 	console.log(error);
+	// });
+	checkForJSON();
+	fs.readFile('./json/data.json', 'utf-8', function(err, data) {
+		if (err) throw err
+		var pacData = JSON.parse(data);
+		sendJSON(res);
+	})
 
 });
+
+function sendJSON(res) {
+	var filePath = './json/data.json'
+	var resolvedPath = path.resolve(filePath);
+	return res.sendFile(resolvedPath);
+}
+
+function checkForJSON() {
+	if (!fs.existsSync('./json/data.json')){
+		fs.writeFile('./json/data.json', '{"lastUpdated":[],"pacspending":[]}', function (err) {
+			if (err) throw err;
+			console.log('data.json has been created.');
+		});
+	}
+};
 
 module.exports = router;
