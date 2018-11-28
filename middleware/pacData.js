4749785293c70848155b1faccec925ea7f253da7
ZHamburglar <kollinbrandenburg@gmail.com>
2018-11-27 20:52:51 -0600

    write file async call

--- middleware/pacData.js
+++ middleware/pacData.js
@@ -1,52 +1,67 @@
-var express = require('express');
+const express = require('express');
 const axios = require('axios');
-var path = require('path');
-var fs = require('fs');
+const util = require('util');
+const path = require('path');
+const fs = require('fs');
+
+const existsSync = util.promisify(fs.existsSync);
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
 module.exports = function(){
     checkForJSON();
-    updateCheck();
+    // updateCheck();
     console.log('this is the middleware');
 }
 
-function writeUpdate (cData) {
-	fs.writeFile('json/data.json', JSON.stringify(cData), 'utf-8', function(err) {
-		if (err) throw err
-		console.log('JSON Sent!');
-	});
-}
+// function writeUpdate (cData) {
+// 	fs.writeFile('json/data.json', JSON.stringify(cData), 'utf-8', function(err) {
+// 		if (err) throw err
+// 		console.log('JSON Sent!');
+// 	});
+// }
 
-function callCOA (cData) {
-	axios.get('https://data.austintexas.gov/resource/asyh-u6ja.json?$limit=5')
-	.then(function (response) {
-		console.log(response.data);
-		cData.pacspending = response.data;
-		writeUpdate(cData);
+// function callCOA (cData) {
+// 	axios.get('https://data.austintexas.gov/resource/asyh-u6ja.json?$limit=5')
+// 	.then(function (response) {
+// 		console.log(response.data);
+// 		cData.pacspending = response.data;
+// 		writeUpdate(cData);
+// 	})
+// 	.catch(function (error) {
+// 		console.log(error);
+// 	});
+// }
+
+// function updateCheck () {
+//     fs.readFile('json/data.json', 'utf-8', function(err, data) {
+//         if (err) throw err
+//         var cData = JSON.parse(data)
+//         lastUpdate = cData.lastUpdated
+//         if ((Date.now() - lastUpdate) > 1000) {
+// 			cData.lastUpdated = [];
+// 			cData.pacspending = [];
+// 			cData.lastUpdated.push(Date.now());
+// 			callCOA(cData);
+//         }
+// 	});
+// };
+
+function writeFilePromise(writePath, textContent) {
+	console.log('writeFilePromise')
+	return new Promise((resolve, reject) => {
+		fs.writeFile(writePath, textContent, (err) => {
+			if (err)
+				reject (err);
+			else
+				resolve();
+		})
 	})
-	.catch(function (error) {
-		console.log(error);
-	});
 }
 
-function updateCheck () {
-    fs.readFile('json/data.json', 'utf-8', function(err, data) {
-        if (err) throw err
-        var cData = JSON.parse(data)
-        lastUpdate = cData.lastUpdated
-        if ((Date.now() - lastUpdate) > 1000) {
-			cData.lastUpdated = [];
-			cData.pacspending = [];
-			cData.lastUpdated.push(Date.now());
-			callCOA(cData);
-        }
-	});
-};
-
-function checkForJSON() {
-	if (!fs.existsSync('json/data.json')){
-		fs.writeFile('json/data.json', '{"lastUpdated":[],"pacspending":[]}', function (err) {
-			if (err) throw err;
-			console.log('data.json has been created.');
-		});
+async function checkForJSON() {
+	const fileExists = fs.existsSync('json/data.json')
+	if (!fileExists){
+		await writeFilePromise('json/data.json', '{"lastUpdated":[],"pacspending":[]}')
 	}
 };
