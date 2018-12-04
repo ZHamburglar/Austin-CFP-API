82c4ea08bcf06973f99cf22e93648506a31d79fc
ZHamburglar <kollinbrandenburg@gmail.com>
2018-12-04 13:09:07 -0600

    eslint added fixed eslint errors.

--- middleware/checkUpdate.js
+++ middleware/checkUpdate.js
@@ -1,14 +1,12 @@
-const express = require('express');
 const axios = require('axios');
 const path = require('path');
 const fs = require('fs');
 
-var router = express.Router();
-
 // Local Paths
 const jsonPath = 'json/data.json';
 
-router.get('/', function(req, res, next) {
+module.exports = function(res){
+	// console.log('checking for file exist')
 	const fileExists = fs.existsSync(jsonPath);
 	if (!fileExists){
 		// If file doesn't exist run this.
@@ -17,7 +15,7 @@ router.get('/', function(req, res, next) {
 		// If file exists run this.
 		foo(res);
 	}
-});
+};
 
 const sendJson = async (res) => {
 	var filePath = './json/data.json';
@@ -26,6 +24,7 @@ const sendJson = async (res) => {
 };
 
 const callAPI = async (x) => {
+	// console.log('calling the API')
 	return new Promise((resolve, reject) => {
 		axios.get('https://data.austintexas.gov/resource/sf6w-qpmi.json?$limit=100&$offset=' + x)
 			.then( function (response) {
@@ -60,6 +59,7 @@ const writeFilePromise = async (writePath, textContent) => {
 };
 
 const foo = async (res) => {
+	// console.log('foo being called');
 	let currentJson = await readFilePromise(jsonPath);
 	let cData = JSON.parse(currentJson);
 	let lastUpdate = cData.lastUpdated;
@@ -81,16 +81,8 @@ const foo = async (res) => {
 };
 
 const bar = async (res) => {
-	let result = [];
-	let offset = 0;
-	// console.log('result.length', result.length, (await callAPI(offset)).length)
-	while ((await callAPI(offset)).length > 99 && offset < 1001){
-		result = result.concat(await callAPI(offset));
-		offset += 100;
-	}
+	let result = await callAPI(0);
 	let cData = {'lastUpdated':[Date.now()],'pacspending':result};
 	await writeFilePromise(jsonPath, JSON.stringify(cData));
 	await sendJson(res);
 };
-
-module.exports = router;
--- routes/name.js
+++ routes/name.js
@@ -1,22 +1,24 @@
-var express = require('express');
+const express = require('express');
 const fs = require('fs');
-const util = require('util');
 const moment = require ('moment');
 
 var router = express.Router();
 
-const readFile = util.promisify(fs.readFile);
+// Middlewares
+// var checkUpdate = require('../middleware/checkUpdate');
 
-const jsonPath = 'json/data.json'
+// Local Paths
+const jsonPath = 'json/data.json';
 
-router.get('/:name', function(req, res, next) {
-	let name = req.params.name
+router.get('/:name', async (req, res, next) => {
+	let name = req.params.name;
 	searchNames(res, name);
 });
 
 const getName = (obj, key, val) => {
+	
 	var objects = [];
-    for (var i in obj) {
+	for (var i in obj) {
 		if (!obj.hasOwnProperty(i)) continue;
 		// This splits up the objects in the JSON into lower levels
 		if (typeof obj[i] == 'object') {
@@ -26,21 +28,23 @@ const getName = (obj, key, val) => {
 			//if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
 			var regex = new RegExp('' + val + '', 'i');
 			// at this point, the line above is the same as: var regex = /${val}/i;
+
 			if (i == key && val == '') {
 				objects.push(obj);
 			}
 			else if (i == key && obj[i].search(regex) != -1) {
+				let newObj;
 				newObj = Object.assign({});
-				newObj.date_reported = moment(obj.date_reported).format('MM-DD-YYYY')
-				newObj.expenditure_type = obj.expenditure_type
-				newObj.expense_description = obj.expense_description
-				newObj.paid_by = obj.paid_by
-				newObj.payee = obj.payee
-				newObj.payee_address = obj.payee_address + ', ' + obj.city_state_zip
-				newObj.payee_type = obj.payee_type
-				newObj.payment_amount = obj.payment_amount
-				newObj.payment_date = moment(obj.payment_date).format('MM-DD-YYYY')
-				newObj.view_report = obj.view_report
+				newObj.date_reported = moment(obj.date_reported).format('MM-DD-YYYY');
+				newObj.expenditure_type = obj.expenditure_type;
+				newObj.expense_description = obj.expense_description;
+				newObj.paid_by = obj.paid_by;
+				newObj.payee = obj.payee;
+				newObj.payee_address = obj.payee_address + ', ' + obj.city_state_zip;
+				newObj.payee_type = obj.payee_type;
+				newObj.payment_amount = obj.payment_amount;
+				newObj.payment_date = moment(obj.payment_date).format('MM-DD-YYYY');
+				newObj.view_report = obj.view_report;
 				objects.push(newObj);
 			} else if (obj[i] == val && key == ''){	
 				//only add if the object is not already in the array
@@ -50,26 +54,25 @@ const getName = (obj, key, val) => {
 			}
 		}
 	}
-    return objects;
-}
+	return objects;
+};
 
 const readFilePromise = async (filepath) => {
-	console.log('readFilePromise')
 	return new Promise((resolve, reject) => {
 		fs.readFile(filepath, function(err, data) {
 			if (err)
 				reject (err);
 			else
 				resolve(data);
-		})
-	})
-}
+		});
+	});
+};
 
 const searchNames = async (res, name) => {
 	let results = await readFilePromise(jsonPath);
-	let result = JSON.parse(results)
-	let names = getName(result.pacspending, 'payee', name)
+	let result = JSON.parse(results);
+	let names = getName(result.pacspending, 'payee', name);
 	res.send(names);
-}
+};
 
 module.exports = router;
